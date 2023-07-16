ed775494598231e9801a938da07c8b9a93c8a6af
vng39etich <vng39etich@gmail.com>
2023-07-16 18:44:07 +0000

    conditional function

---
+++ conditions.py
@@ -0,0 +1,13 @@
+#!usr/bin/pythom3
+
+def confirm_number(num):
+    
+    if num > 0:
+        print("The number is positive")
+    elif num < 0:
+        print("The number is negative")
+    else:
+        print("The number is zero")
+
+
+
